1e516bedd081c6270738bedca20309a3b040d8ba
mechaneer31 <225097583+mechaneer31@users.noreply.github.com>
2026-03-23 11:52:28 -0700

    Created Orders page, added html and bound to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage';
 
 import './App.css'
 
 function App() {
-  
+
 
   return (
     <>
@@ -19,11 +20,16 @@ function App() {
           path="checkout"
           element={<CheckoutPage />}
         />
-        
+
+        <Route
+          path="orders"
+          element={<OrdersPage />}
+        />
+
       </Routes>
 
 
-      
+
     </>
   )
 }
