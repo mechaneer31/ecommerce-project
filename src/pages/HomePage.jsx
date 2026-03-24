d27a8545da4ef037718b66762f0cf9e7c0ab5670
mechaneer31 <225097583+mechaneer31@users.noreply.github.com>
2026-03-24 13:12:17 -0700

    added func for products to be displayed by fetch from backend

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,21 @@
 import axios from 'axios';
-import { Header } from '../components/Header'
+import { useEffect, useState } from 'react';
+import { Header } from '../components/Header';
 import './HomePage.css';
-import { products } from '../../starting-code/data/products'
-
-import CheckmarkImage from '../assets/images/icons/checkmark.png'
+import CheckmarkImage from '../assets/images/icons/checkmark.png';
 
 export function HomePage() {
 
-    axios.get('http://localhost:3000/api/products')
-        .then((response) => {
-            console.log(response.data)
-        });
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3000/api/products')
+            .then((response) => {
+                setProducts(response.data)
+            });
+    }, []);
+
+
 
     return (
         <>
