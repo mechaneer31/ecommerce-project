a6fb56840c12960176ba9c2a107fbbef71e79c8d
mechaneer31 <225097583+mechaneer31@users.noreply.github.com>
2026-03-25 12:08:27 -0700

    updated app/homepage/checkout/orders axios function to async await

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -11,10 +11,12 @@ export function HomePage({ cart }) {
 
 
     useEffect(() => {
-        axios.get('/api/products')
-            .then((response) => {
-                setProducts(response.data)
-            });
+        const getHomeData = async () => {
+            const response = await axios.get('/api/products');
+            setProducts(response.data);
+        };
+
+        getHomeData();
     }, []);
 
     return (
