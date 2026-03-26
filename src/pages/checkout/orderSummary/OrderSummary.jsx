186cd90af0283bea3b88fe181390a592613203c8
mechaneer31 <225097583+mechaneer31@users.noreply.github.com>
2026-03-26 16:08:24 -0700

    forgot to go to branch, added delete cart item functionality

--- src/pages/checkout/orderSummary/OrderSummary.jsx
+++ src/pages/checkout/orderSummary/OrderSummary.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import { Fragment } from 'react'
 import { DeliveryOptions } from './DeliveryOptions';
 import { CartItemDetails } from './CartItemDetails';
@@ -10,10 +11,18 @@ export function OrderSummary({ cart, deliveryOptions, loadCart }) {
         <div className="order-summary">
             {deliveryOptions.length > 0 && cart.map((cartItem) => {
 
+                const deleteCartItem = async () => {
+                    await axios.delete(`/api/cart-items/${cartItem.productId}`);
+                    await loadCart();
+                };
+
+
                 return (
                     <Fragment
                         key={cartItem.productId}
                     >
+
+
 
                         <div className="cart-item-container">
 
@@ -23,9 +32,12 @@ export function OrderSummary({ cart, deliveryOptions, loadCart }) {
                                 deliveryOptions={deliveryOptions}
                             />
 
+
                             <div className="cart-item-details-grid">
                                 <CartItemDetails
                                     cartItem={cartItem}
+                                    loadCart={loadCart}
+                                    deleteCartItem={deleteCartItem}
                                 />
 
 
@@ -33,7 +45,6 @@ export function OrderSummary({ cart, deliveryOptions, loadCart }) {
                                 <DeliveryOptions
                                     cartItem={cartItem}
                                     deliveryOptions={deliveryOptions}
-                                    loadCart={loadCart}
                                 />
 
                             </div>
